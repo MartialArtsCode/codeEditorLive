84d370f60a38557b47d661ff1726a3819b4def18
MartialArtsDevelopmentLab <ptacademy@duck.com>
2026-03-15 03:08:36 -0400

    Refactor draw function for better readability

--- vusualization/dependencyGraph.js
+++ vusualization/dependencyGraph.js
@@ -1,46 +1,37 @@
-import {project} from "../core/projectManager.js"
-  
-const canvas=document.getElementById("graph")
-  const ctx=canvas.getContext("2d")
+import { project } from "../core/projectManager.js";
 
-function draw(){
+const canvas = document.getElementById("graph");
+const ctx = canvas.getContext("2d");
 
-  ctx.clearRect(0,0,canvas.width,canvas.height)
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
 
-  ctx.fillStyle="white"
+    ctx.fillText("HTML", 40, 100);
+    ctx.fillText("CSS", 160, 40);
+    ctx.fillText("JS", 160, 160);
+    ctx.fillText("API", 260, 100);
 
-  ctx.fillText("HTML",40,100)
-    ctx.fillText("CSS",160,40)
-      ctx.fillText("JS",160,160)
-        ctx.fillText("API",260,100)
+    if (project.files.css.length > 0) {
+        ctx.beginPath();
+        ctx.moveTo(60, 100);
+        ctx.lineTo(160, 40);
+        ctx.stroke();
+    }
 
-  if(project.files.css.length>0){
+    if (project.files.js.length > 0) {
+        ctx.beginPath();
+        ctx.moveTo(60, 100);
+        ctx.lineTo(160, 160);
+        ctx.stroke();
+    }
 
-    ctx.beginPath()
-      ctx.moveTo(60,100)
-        ctx.lineTo(160,40)
-          ctx.stroke()
-
-  }
-
-  if(project.files.js.length>0){
-
-    ctx.beginPath()
-      ctx.moveTo(60,100)
-        ctx.lineTo(160,160)
-          ctx.stroke()
-
-  }
-
-  if(project.files.js.includes("fetch")){
-
-    ctx.beginPath()
-      ctx.moveTo(160,160)
-        ctx.lineTo(260,100)
-          ctx.stroke()
-
-  }
-
+    if (project.files.js.includes("fetch")) {
+        ctx.beginPath();
+        ctx.moveTo(160, 160);
+        ctx.lineTo(260, 100);
+        ctx.stroke();
+    }
 }
 
-setInterval(draw,1500)
+setInterval(draw, 1500);
