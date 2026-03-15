2f0b300873f56bee05afa29eaeb2b5d2e8e5f125
MartialArtsDevelopmentLab <ptacademy@duck.com>
2026-03-15 02:54:19 -0400

    Refactor editorPanel.js for consistency and clarity

--- ui/editorPanel.js
+++ ui/editorPanel.js
@@ -1,25 +1,19 @@
-import {project} from "../core/projectManager.js"
+import { project } from "../core/projectManager.js";
 
 require.config({
-    paths:{vs:"https://unpkg.com/monaco-editor@0.45.0/min/vs"}
-})
+    paths: { vs: "https://unpkg.com/monaco-editor@0.45.0/min/vs" }
+});
 
-require(["vs/editor/editor.main"],()=>{
-
-    editor = monaco.editor.create(document.getElementById("editor"), {
-
+require(["vs/editor/editor.main"], () => {
+    const editor = monaco.editor.create(document.getElementById("editor"), {
         value: project.files.html,
         language: "html",
         theme: "vs-dark",
-        automaticLayout: true,  // important
+        automaticLayout: true, // important
         tabSize: 4
     });
 
-
-    editor.onDidChangeModelContent(()=>{
-
-        project.files[project.active]=editor.getValue()
-
-    })
-
-})
+    editor.onDidChangeModelContent(() => {
+        project.files[project.active] = editor.getValue();
+    });
+});
