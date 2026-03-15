379b9d07beedb1882c4be4c6e1cd0b2a60739d07
MartialArtsDevelopmentLab <ptacademy@duck.com>
2026-03-15 01:07:49 -0400

    Implement Tab key behavior for text editor
    Add Tab key functionality to insert spaces in editor

--- ui/editorPanel.js
+++ ui/editorPanel.js
@@ -1,3 +1,23 @@
+const editor = document.getElementById("editor");
+
+editor.addEventListener("keydown", function(e) {
+
+    if (e.key === "Tab") {
+        e.preventDefault();
+
+        const start = this.selectionStart;
+        const end = this.selectionEnd;
+
+        this.value =
+            this.value.substring(0, start) +
+            "    " +
+            this.value.substring(end);
+
+        this.selectionStart = this.selectionEnd = start + 4;
+    }
+
+});
+
 import {project,updateFile} from "../core/projectManager.js"
 
 const editor=document.getElementById("editor")
