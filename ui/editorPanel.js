87165b39d18075897836f04a77fc92753de9c39d
MartialArtsDevelopmentLab <ptacademy@duck.com>
2026-03-15 02:14:46 -0400

    Refactor editor event handling and integrate Monaco Editor

--- ui/editorPanel.js
+++ ui/editorPanel.js
@@ -1,61 +1,29 @@
-import {project,updateFile} from "../core/projectManager.js"
+import {project} from "../core/projectManager.js"
 
-const editor=document.getElementById("editor")
+require.config({
+    paths:{vs:"https://unpkg.com/monaco-editor@0.45.0/min/vs"}
+})
 
-editor.addEventListener("input",()=>{
+require(["vs/editor/editor.main"],()=>{
 
-updateFile(project.activeFile,editor.value)
+    window.editor=monaco.editor.create(document.getElementById("editor"),{
+
+        value:project.files.html,
+
+        language:"html",
+
+        theme:"vs-dark",
+
+        automaticLayout:true,
+
+        tabSize:4
+
+    })
+
+    editor.onDidChangeModelContent(()=>{
+
+        project.files[project.active]=editor.getValue()
+
+    })
 
 })
-const editor = document.getElementById("editor");
-
-editor.addEventListener("keydown", function(e) {
-
-    if (e.key === "Tab") {
-        e.preventDefault();
-
-        const start = this.selectionStart;
-        const end = this.selectionEnd;
-
-        this.value =
-            this.value.substring(0, start) +
-            "    " +
-            this.value.substring(end);
-
-        this.selectionStart = this.selectionEnd = start + 4;
-    }
-
-});
-
-editor.addEventListener("keydown", function(e) {
-
-    if (e.key === "Tab") {
-
-        e.preventDefault();
-
-        const start = this.selectionStart;
-        const end = this.selectionEnd;
-
-        if (e.shiftKey) {
-
-            if (this.value.substring(start-4,start) === "    ") {
-
-                this.value =
-                    this.value.substring(0,start-4) +
-                    this.value.substring(end);
-
-                this.selectionStart = this.selectionEnd = start-4;
-            }
-
-        } else {
-
-            this.value =
-                this.value.substring(0,start) +
-                "    " +
-                this.value.substring(end);
-
-            this.selectionStart = this.selectionEnd = start+4;
-        }
-    }
-
-});
